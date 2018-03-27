1ad36a5c6d7ed5ea0486fb3d80031d588b5e0df6
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-03-27 16:03:01 +0300

    On Lesson 141

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,43 @@
-import React from "react";
-import Aux from '../../hoc/Auxiliary';
-import classes from './Layout.css'; 
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    {/* <div>Toolbar, Sidebar, Backdrop</div> */}
-    <SideDrawer />
+  sideDrawerToggleHandler = () => {
+    // seState() is asynchronous and to pass this.state.showSideDrawer 
+    // with no issue we have to use prevState
+    // switch between false and true
+    // prevState is passed by default
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
 
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerTooggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer 
+          open={this.state.showSideDrawer} 
+          closed={this.sideDrawerCloseHandler} />
+        <main className={classes.Content}>
+          {/* output the component we wrap this layout 
+        so use this layout as wrapper around the core content
+        that we want to render to the screen*/}
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
 
-export default layout;
+export default Layout;
